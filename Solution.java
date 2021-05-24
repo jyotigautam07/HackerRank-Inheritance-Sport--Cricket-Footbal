b6705054ac6e33b2ea462c340e726934d95502a8
jyotigautam07 <84512838+jyotigautam07@users.noreply.github.com>
2021-05-24 20:10:03 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,101 @@
+import java.util.*;
+import java.text.*;
+/*
+Write the implementation of the Cricket and Football classes
+*/
+interface Sport
+{
+    default void calculateAvgAge(int[] age){
+        double sum=0;
+        for(int age1=0;age1<age.length;age1++){
+            
+            sum=sum+age[age1];
+        }
+        //System.out.println("sum "+sum);
+        double avg = sum/age.length;
+        
+        System.out.println("The average age of the team is "+new DecimalFormat("####.##").format(avg));
+        
+    }
+List<Integer> retired = new ArrayList<Integer>();
+    default void retirePlayer(int id){
+        
+        if(!retired.contains(id)){
+            if(id>=20 ){
+            System.out.println("Player with id: "+id+" has retired");
+           }
+        else if( id<=40){
+            System.out.println("Player with id: "+id+" has retired");
+        }
+        }
+        else{
+            System.out.println("Player has already retired");
+        }
+        
+        retired.add(id);
+    }
+    default void playerTransfer(int fee, int id){
+        
+       
+            System.out.println("Player with id: "+id+" has been transferred with a fee of "+fee);
+   
+}
+}
+class Cricket implements Sport{
+    Cricket(){
+        System.out.println("A new cricket team has been formed");
+    }
+    
+}
+class Football implements Sport{
+    
+    Football(){
+        System.out.println("A new football team has been formed");
+    }
+}
+
+public class Solution{
+ public static void main(String[] args){
+  Scanner sc = new Scanner(System.in);
+
+  Cricket c = new Cricket();
+  Football f = new Football();
+
+  int[] age1 = new int[11];
+  int[] age2 = new int[11];
+
+  for(int i = 0; i < 2; i++)
+   {
+    String[] age = sc.nextLine().split(" ");
+    if(i == 0){
+    int j = 0;
+    for(String s : age)
+     age1[j++] = Integer.parseInt(s);
+    }
+    else{
+     int j = 0;
+     for(String s : age)
+      age2[j++] = Integer.parseInt(s);  
+    }
+   }
+   c.calculateAvgAge(age1);
+   f.calculateAvgAge(age2);
+
+   for(int i = 0; i < 6; i++){
+   
+   if(i < 3){
+    int x = Integer.parseInt(sc.nextLine());
+
+    c.retirePlayer(x);
+   }
+   else if(i < 5){
+    int x = Integer.parseInt(sc.nextLine());
+    f.retirePlayer(x);  
+   }
+   else {
+    String[] temp = sc.nextLine().split(" ");  
+    f.playerTransfer(Integer.parseInt(temp[0]), Integer.parseInt(temp[1]));
+  } 
+ }  
+ }
+}
